a691e91c363b91f11b6c0c6f75b2804cb35e82fe
Jasmine PB <jashmipaare@gmail.com>
2018-08-29 16:15:46 +0530

    calculator test

---
+++ src/main/java/com/teamsankya/calculatortest/App.java
@@ -0,0 +1,32 @@
+package com.teamsankya.calculatortest;
+
+import com.teamsankya.calculator11.Addition;
+import com.teamsankya.calculator11.Division;
+import com.teamsankya.calculator11.Multiplication;
+import com.teamsankya.calculator11.Subtraction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	int sum = Addition.add(10,20);
+        System.out.println( sum );
+        
+        long sum2 = Addition.add(10,40);
+        System.out.println( sum2 );
+        
+        
+        int sub = Subtraction.subtract(20, 10);
+        System.out.println( sub );
+        
+        int mul = Multiplication.multiply(25, 5);
+        System.out.println( mul );
+         
+        int div = Division.division(12, 6);
+        System.out.println( div );
+    }
+}
